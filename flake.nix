07938f07810a9dff8112332c1d06bf33f0898ede
drzln <luis.d@akeyless.io>
2026-03-30 14:48:17 -0400

    add forge input for standard image release pattern
    Wire up forge as a flake input and pass it through to tool-image-flake.nix
    so that `nix run .#release` uses the standard forge-based image release
    with skopeo/regctl instead of custom docker push scripts. Also enable
    both amd64 and arm64 architectures, and follow nixpkgs through fenix
    and crate2nix inputs to avoid closure duplication.

--- flake.nix
+++ flake.nix
@@ -3,23 +3,38 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    crate2nix.url = "github:nix-community/crate2nix";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crate2nix = {
+      url = "github:nix-community/crate2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
     substrate = {
       url = "github:pleme-io/substrate";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.fenix.follows = "fenix";
+    };
+    forge = {
+      url = "github:pleme-io/forge";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.fenix.follows = "fenix";
+      inputs.substrate.follows = "substrate";
+      inputs.crate2nix.follows = "crate2nix";
     };
   };
 
-  outputs = { self, nixpkgs, crate2nix, flake-utils, substrate, ... }:
-    (import "${substrate}/lib/rust-service-flake.nix" {
-      inherit nixpkgs crate2nix flake-utils;
+  outputs = { self, nixpkgs, crate2nix, flake-utils, substrate, forge, ... }:
+    (import "${substrate}/lib/build/rust/tool-image-flake.nix" {
+      inherit nixpkgs crate2nix flake-utils forge;
     }) {
       toolName = "image-sync";
       src = self;
       repo = "pleme-io/image-sync";
-
-      # Include crane binary in the Docker image for registry operations
-      extraPackages = pkgs: [ pkgs.crane ];
+      tag = "0.1.0";
+      extraContents = pkgs: [ pkgs.crane ];
+      architectures = ["amd64" "arm64"];
     };
 }
